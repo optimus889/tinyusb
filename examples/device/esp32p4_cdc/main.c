2539e844adcc951485167917ec819e9f4cea3e79
optimus889 <132802487+optimus889@users.noreply.github.com>
2025-05-17 00:05:30 +0800

    Add files via upload

--- examples/device/esp32p4_cdc/main.c
+++ examples/device/esp32p4_cdc/main.c
@@ -1,14 +1,18 @@
+#include "bsp/board.h"
 #include "tusb.h"
 
-int main(void)
-{
-  board_init();
-  tusb_init();
+int main(void) {
+    board_init();
+    tusb_init();
 
-  while (1)
-  {
-    tud_task(); // tinyusb device task
-  }
+    while (1) {
+        tud_task();
 
-  return 0;
+        if (tud_cdc_n_available(0)) {
+            uint8_t buf[64];
+            uint32_t count = tud_cdc_n_read(0, buf, sizeof(buf));
+            tud_cdc_n_write(0, buf, count);
+            tud_cdc_n_write_flush(0);
+        }
+    }
 }
